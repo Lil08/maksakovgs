7a3df757fe39480c784498f0465d9249c757426a
olgamaksakova <maksakovaolga28@gmail.com>
2022-04-29 13:46:02 +0300

    fix content map

--- views/site/contact.php
+++ views/site/contact.php
@@ -26,8 +26,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="container">
     <div class="row">
         <div class="map">
-            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Af6594ea3e751bee67cd5f6201927fc80daad76e063f0691bf8fc5dd846ea6ca1&amp;source=constructor"
-                    width="100%" height="400" frameborder="0"></iframe>
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Aebf4dc9103444ce1479d662c7e18f3adda0de01545aba3a99c3394d8767951af&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
         </div>
     </div>
 </div>
@@ -48,7 +47,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <div class="col-lg-6">
             <div class="recent">
-                <button class="btn-primarys"><h3>Свяжитесь с нами</h3></button>
+                <button class="btn-primarys"><h3>Свяжитесь со мной</h3></button>
             </div>
             <?= ContactWidget::widget() ?>
         </div>
