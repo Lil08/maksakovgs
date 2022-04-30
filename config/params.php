4a7bcc6e840697fc5ada16c11b54955eef158389
olgamaksakova <maksakovaolga28@gmail.com>
2022-04-30 10:57:12 +0300

    fix params email

--- config/params.php
+++ config/params.php
@@ -1,12 +1,8 @@
 <?php
 //ПРАВИТЬ
 return [
-//    'adminEmail' => 'maksakow28@yandex.ru',
-//    'senderEmail' => 'maksakow28@yandex.ru',
-//    'adminEmail' => 'maksakovaolga28@gmail.com',
-//    'senderEmail' => 'maksakovaolga28@gmail.com',
-    'adminEmail' => 'maksakovaolga7@yandex.ru',
-    'senderEmail' => 'maksakovaolga7@yandex.ru',
+    'adminEmail' => 'maksakow28@yandex.ru',
+    'senderEmail' => 'maksakow28@yandex.ru',
 
     'senderName' => 'Сайт адвоката Максаков Г.С.',
 ];
